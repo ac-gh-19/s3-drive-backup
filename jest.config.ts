3599dcf40c69ab1694502620ec3efc039be54b25
ac-gh-19 <chanandrew1925@gmail.com>
2026-01-23 02:36:48 -0800

    update node version / packages

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest', 
+  },
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts', '<rootDir>/__tests__/**/*.spec.ts'],
+};
