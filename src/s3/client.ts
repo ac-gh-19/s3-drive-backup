4a473749ab34ea48f5170a37fd288cfe6ddc4c7b
ac-gh-19 <chanandrew1925@gmail.com>
2026-01-24 15:58:37 -0800

    add access to s3Client

---
+++ src/s3/client.ts
@@ -0,0 +1,17 @@
+import { S3, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+export async function getS3Client(): Promise<S3> {
+    return new S3({ region: "us-west-1" });
+}
+
+export async function listBuckets(s3Client: S3) {
+    const input = {
+        ContinuationToken: undefined,
+        MaxDirectoryBuckets: 10,
+    };
+
+    const command = new ListBucketsCommand(input);
+    const response = await s3Client.send(command);
+    console.log(response);
+    return response;
+}
