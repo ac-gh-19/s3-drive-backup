e672b8b6c19355cc6ebeda916c8f69b51cc91ecc
ac-gh-19 <chanandrew1925@gmail.com>
2026-01-25 19:37:26 -0800

    change s3Key - see comment for reason

--- src/utils/computeS3Key.ts
+++ src/utils/computeS3Key.ts
@@ -1,8 +1,13 @@
 import { DriveFile } from "../types/driveFile";
 
 export function computeS3Key(file: DriveFile): string {
-  const splitDate = file.createdTime!.split("-");
+  //   const splitDate = file.createdTime!.split("-");
 
-  const date = `${splitDate[0]}/${splitDate[1]}/${splitDate[2].slice(0, 2)}`;
-  return `${date}/${file.md5Checksum!}/${file.id}/${file.name}`;
+  //   const date = `${splitDate[0]}/${splitDate[1]}/${splitDate[2].slice(0, 2)}`;
+  //   return `${date}/${file.md5Checksum!}/${file.id}/${file.name}`;
+
+  // The date-based folder structure caused issues with folder paths
+  // in S3 as created_time is based on when file was uploaded to Drive
+  // and not when the photo was taken by user. TODO: revisit later if needed
+  return `${file.md5Checksum!}-${file.id}/${file.name}`;
 }
